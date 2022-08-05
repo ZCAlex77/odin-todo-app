eeb64036721e80aee07aae1feff774426795417f
Alex <alexandruzmau320@gmail.com>
2022-08-05 10:32:03 +0300

    Initial commit

---
+++ dist/bundle.485698cafd74cdb78fbf.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('All good!');
+/******/ })()
+;
+//# sourceMappingURL=bundle.485698cafd74cdb78fbf.js.map
